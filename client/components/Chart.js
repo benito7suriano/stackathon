07f9a7b18074e8a6ca688e9c8bf858257e7ce726
Beno Suriano <bjsuriano@gmail.com>
2018-04-29 00:56:37 -0600

    Refactored Main to func component.

--- client/components/Chart.js
+++ client/components/Chart.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import generateGraph from '../../utils/generateGraph'
 
+const DUMMY_SHIT = [
+  { id: 0, reading: 10 },
+  { id: 1, reading: 15 },
+  { id: 2, reading: 35 },
+  { id: 3, reading: 7 },
+  { id: 4, reading: 30 },
+  { id: 5, reading: 150 },
+  { id: 6, reading: 77 }
+]
+
 export default class Chart extends React.Component {
   constructor() {
     super()
@@ -12,17 +22,27 @@ export default class Chart extends React.Component {
         right: 20,
         bottom: 20,
         left: 50
-      }
+      },
+      readings: DUMMY_SHIT
     }
   }
 
   componentDidMount() {
-    const { lineData } = this.props
-    const WIDTH = this.state.width
-    const HEIGHT = this.state.height
-    const MARGINS = this.state.margins
+    try {
+      const lineData = this.state.readings
+      const WIDTH = this.state.width
+      const HEIGHT = this.state.height
+      const MARGINS = this.state.margins
 
-    generateGraph(lineData, MARGINS, HEIGHT, WIDTH)
+      // const { data } = await axios.get('/api/pot-readings')
+      // console.log('Got this data: ', data)
+      // const readings = data
+      // this.setState({ readings })
+
+      generateGraph(lineData, MARGINS, HEIGHT, WIDTH)
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   render() {
--- client/components/Main.js
+++ client/components/Main.js
@@ -1,31 +1,13 @@
 import React from 'react'
 import Chart from './Chart'
 
-const DUMMY_SHIT = [
-  { id: 0, reading: 10 },
-  { id: 1, reading: 15 },
-  { id: 2, reading: 35 },
-  { id: 3, reading: 7 },
-  { id: 4, reading: 30 },
-  { id: 5, reading: 150 },
-  { id: 6, reading: 77 }
-]
-
-export default class Main extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      readings: DUMMY_SHIT
-    }
-  }
-
-  render() {
-    console.log('State before rendering: ', this.state.readings)
-    return (
+const Main = () => {
+  return (
       <div id="demo" className="bg-gray-c column center-x fill-xy">
         <h1>Dashboard</h1>
-        <Chart lineData={this.state.readings} />
+        <Chart />
       </div>
     )
   }
-}
+
+export default Main
